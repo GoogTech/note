22ecfafde1180e6b14c8eac398483de970dc343b
googtech <hi@goog.tech>
2021-09-07 19:22:24 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     lang: 'zh-CN',
     title: '你好， VuePress ！',
-    base: '/note-googtech/',
+    base: '/note/',
     description: '这是我的第一个 VuePress 站点',
 
     themeConfig: {
