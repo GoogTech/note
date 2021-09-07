1418ff59df9405337b29a7dcb6f106e5e9f41697
googtech <hi@goog.tech>
2021-09-07 18:17:45 +0800

    :tada: init vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: '你好， VuePress ！',
+    base: '/note-googtech/',
+    description: '这是我的第一个 VuePress 站点',
+
+    themeConfig: {
+        // logo: 'https://vuejs.org/images/logo.png',
+        logo: '/images/logo.png'
+    },
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# hello vuepress
+## hey guys
+
+如果你想要把当前页面的目录添加到 Markdown 内容中，你可以使用`[[toc]]`语法
+[[toc]]
+
+![VuePress Logo](/images/logo.png)
