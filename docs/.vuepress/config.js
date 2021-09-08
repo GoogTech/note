212793ad90a7958b30463ba58125da9101e2ca17
googtech <hi@goog.tech>
2021-09-08 09:14:54 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,14 +6,14 @@ module.exports = {
     // SEO
     head: [
         //icon
-        [['link', { rel: 'icon', href: '/favicon.ico' }]],
+        // [['link', { rel: 'icon', href: '/favicon.ico' }]],
         // ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon/apple-touch-icon.png' }],
         // ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icon/favicon-32x32.png' }],
         // ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icon/favicon-16x16.png' }],
         // ['link', { rel: 'manifest', href: '/icon/site.webmanifest' }],
-        
-        ['meta', {name: 'author', content: 'GoogTech'}],
-        ['meta', {name: 'keywords', content: 'GoogTech, 408, CS Course Learning Note, https://goog.tech/note'}],
+
+        ['meta', { name: 'author', content: 'GoogTech' }],
+        ['meta', { name: 'keywords', content: 'GoogTech, 408, CS Course Learning Note, https://goog.tech/note' }],
     ],
     // 搜索
     plugins: [
