dbaef013a4d1c3f6e89c120e24d3ad1a61a628d7
googtech <hi@goog.tech>
2021-09-12 19:43:59 +0800

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     // SEO
     head: [
         //icon
-        ['link', { rel: 'icon', href: '/icon/favicon.ico' }],
+        ['link', { rel: 'icon', href: 'note/icon/favicon.ico' }],
         // ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon/apple-touch-icon.png' }],
         // ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icon/favicon-32x32.png' }],
         // ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icon/favicon-16x16.png' }],
@@ -34,7 +34,7 @@ module.exports = {
 
     themeConfig: {
         // logo
-        logo: 'note/icon/apple-touch-icon.png',
+        logo: '/icon/apple-touch-icon.png',
         // Github
         repo: 'googtech/note',
         repoLabel: 'Github',
